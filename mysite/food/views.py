612114976c160b4a5170b932002843efa6f6e389
Anto1111-stack <anto.vrsalovic@gmail.com>
2023-10-03 10:53:37 +0100

    views and detail views tut

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
+from django.template import loader
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World')
+    item_list = Item.objects.all()
+    #template = loader.get_template('food/index.html')
+    context ={
+        'item_list':item_list,
+
+    }
+    return render(request, 'food/index.html', context)
 
 
 def item(request):
-    return HttpResponse('This is an item view')
+    return HttpResponse('<h1>This is an item view</h1>')
+
+def detail(request,item_id):
+    item = Item.objects.get(pk=item_id)
+    context ={
+        'item':item,
+    }
+    return HttpResponse('This is an item no/id: %' % item_id)
