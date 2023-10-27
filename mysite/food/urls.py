275f45f4b6827c81371a0e3944475b845dbe0737
Anto1111-stack <anto.vrsalovic@gmail.com>
2023-10-27 14:05:33 +0100

    Implemented form

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', views.index, name ='index'),
     #food/1
     path('<int:item_id>/',views.detail,name='detail'),
-    path('item/', views.item, name='item')
+    path('item/', views.item, name='item'),
+    # add items
+    path('add', views.create_item,name='create_item'), 
 ]
